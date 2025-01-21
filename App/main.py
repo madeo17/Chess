f4d7dd688000e4adf66f1196cfd20f858609595a
Mateusz Malecki <matemalec@gmail.com>
2025-01-21 21:55:19 +0100

    Draw chessboard

--- App/main.py
+++ App/main.py
@@ -1,19 +1,49 @@
 import pygame
 
+GREEN = (105, 130, 90)
+WHITE = (245, 250, 210)
+SQUARE_SIZE = 80
+
 class Square(pygame.sprite.Sprite):
 
     def __init__(self):
         super(Square, self).__init__()
-        self.surf = pygame.Surface((25, 25))
-        self.surf.fill((0, 200, 255))
+        self.surf = pygame.Surface((SQUARE_SIZE, SQUARE_SIZE))
+        self.color = WHITE
+        self.surf.fill(self.color)
         self.rect = self.surf.get_rect()
 
+    def draw(self, screen, x, y):
+        screen.blit(self.surf, (x,y))
+
+    def switch_color(self):
+        if self.color == WHITE:
+            self.color = GREEN
+        else:
+            self.color = WHITE
+        self.surf.fill(self.color)
+
+
+def draw_board(screen):
+    square = Square()
+    offset = SQUARE_SIZE
+    def draw_rank(y):
+        nonlocal screen, square, offset
+        x = 50
+        for _ in range(8):
+            square.draw(screen, x, y)
+            x += offset
+            square.switch_color()
+    y = 50
+    for _ in range(8):
+        draw_rank(y)
+        y += offset
+        square.switch_color()
 
 pygame.init()
 screen = pygame.display.set_mode((1200, 800))
-
-square = Square()
-
+draw_board(screen)
+pygame.display.flip()
 
 game_on = True
 while game_on:
@@ -22,8 +52,6 @@ while game_on:
         if event.type == pygame.QUIT:
             game_on = False
 
-    screen.blit(square.surf, (50,50))
-
-    pygame.display.flip()
+    # pygame.display.flip()
 
 pygame.quit()
