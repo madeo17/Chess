87f97746cd2b34d8eb32d8816392ad027c1138af
Mateusz Malecki <matemalec@gmail.com>
2025-01-29 15:38:59 +0100

    Update pieces images

--- App/pieces/piece.py
+++ App/pieces/piece.py
@@ -1,17 +1,25 @@
 import pygame
 from pathlib import Path
+from App.square import SQUARE_SIZE
 
 IMAGE_SIZE = (226 * 0.24, 270 * 0.24)
 
 class Piece:
     def __init__(self, name: str, is_white: bool):
-        color = "white" if is_white else "black"
+        self.is_white = is_white
+        self.__init_image(name)
+        self.rect = self.image.get_rect()
+        self.moved = False
+
+    def __init_image(self, name):
+        color = "white" if self.is_white else "black"
         image_path = Path('img', 'pieces', f'{color}_{name}.png')
         self.image = pygame.image.load(image_path).convert_alpha()
-        self.image = pygame.transform.scale(self.image, IMAGE_SIZE)
-        self.rect = self.image.get_rect()
-        self.is_white = is_white
-        self.moved = False
+        original_width, original_height = self.image.get_size()
+        aspect_ratio = original_width / original_height
+        new_width = SQUARE_SIZE
+        new_height = int(SQUARE_SIZE / aspect_ratio)
+        self.image = pygame.transform.smoothscale(self.image, (new_width, new_height))
 
     def get_image(self):
         return self.image
